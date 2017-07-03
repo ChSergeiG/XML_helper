bd0b1b7d8705e73faa7d268d4214c0f107a93bc7
ChSergeiG <chsergeyg@gmail.com>
2017-07-03 19:00:05 +0300

    v 0.00

--- src/su/mpstsl/XML/Helper.java
+++ src/su/mpstsl/XML/Helper.java
@@ -1,8 +1,45 @@
 package su.mpstsl.XML;
 
-public class Helper {
+import javax.swing.*;
+import java.awt.*;
 
+public class Helper {
+    String headName = "XML helper v 0.01";
+    JFrame frame = new JFrame(headName);
+    BorderLayout layout;
+    JPanel mainPanel;
+    JMenu filemenu;
+    JMenuBar menuBar;
+
+    private void SetUpGUI() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        layout = new BorderLayout();
+        mainPanel = new JPanel(layout);
+        menuBar = new JMenuBar();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        menuBar.add(filemenu);
+
+
+
+
+
+
+
+
+
+
+
+
+        frame.setJMenuBar(menuBar);
+        frame.setBounds(50, 50, 300, 300);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+    }
     public static void main(String[] args) {
         Helper hlp = new Helper();
+        hlp.SetUpGUI();
     }
 }
